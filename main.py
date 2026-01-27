a66fb903613682c247fb7a9bc32e7c085b271c18
muhammadzohair <zohairmuhammad@gmail.com>
2026-01-26 23:04:49 -0500

    added tkinter to project

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from zipfile import ZipFile
-
-import numpy
 import stats_can
 import pandas as pd
 from pandas.core.dtypes.common import INT64_DTYPE
 from py3_wget import download_file
 import numpy as np
+from tkinter import *
 
 '''Uses stats_can to get data on retail prices of specific goods, located in a zipfile 
 containing a csv file and metadata'''
 grocery_table = stats_can.sc.get_full_table_download("18-10-0245-01")
 
-#Uses py3_wget to overwrite data.zip, overwrites file of exists.
+#Uses py3_wget to overwrite data.zip, overwrites file if exists.
 download_file(grocery_table,overwrite=True, output_path="data.zip")
 
 #Reads the first file in data.zip to a pandas dataframe, ignoring the metadata file in the folder.
@@ -22,8 +21,16 @@ with ZipFile("data.zip", 'r') as z:
                                   "COORDINATE":float,"VALUE":float,"STATUS":"string","SYMBOL":"string","TERMINATED":"string",
                                   "DECIMALS":int})
 
+#milk_prices = df.loc[(df["GEO"] == 'Ontario') & (df["Products"] == "Milk, 4 litres")]
+#print(milk_prices.iloc[0]["VALUE"])
+# Python tkinter hello world program
 
 
+root = Tk()
+a = Label(root, text ="Hello World")
+a.pack()
+
+root.mainloop()
 
 
 
